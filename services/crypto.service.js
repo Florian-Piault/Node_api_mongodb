3e94fa213435553a40d763d362b0a50d3b91d165
Florian PIAULT <florian.piault@mail-ecv.fr>
2021-03-18 10:36:05 +0100

    réglage bug

--- services/crypto.service.js
+++ services/crypto.service.js
@@ -1,55 +1,58 @@
 /* 
 Imports
 */
-    let CryptoJS = require("crypto-js");
+let CryptoJS = require('crypto-js');
 //
 
 /*
 Service definition
 */
-    const cryptData = (input, type = 'string') => {
-        // Encrypt
-        let output = undefined;
+const cryptData = (input, type = 'string') => {
+	// Encrypt
+	let output = undefined;
+	// Switch data type
+	switch (type) {
+		case 'string':
+			output = CryptoJS.AES.encrypt(input, process.env.CRYPTO_KEY);
+			break;
 
-        // Switch data type
-        switch(type){
-            case 'string':
-            output = CryptoJS.AES.encrypt(input, process.env.CRYPTO_KEY);
-            break;
+		case 'object':
+			output = CryptoJS.AES.encrypt(
+				JSON.stringify(input),
+				process.env.CRYPTO_KEY,
+			);
+			break;
+	}
 
-            case 'object':
-            output = CryptoJS.AES.encrypt(JSON.stringify(input), process.env.CRYPTO_KEY);
-            break;
-        }
+	// Return crypted output
+	return output.toString();
+};
 
+const decryptData = (item, ...inputs) => {
+	// Loop on values
+	for (let value of inputs) {
+		// Loop on object
+		for (let prop in item) {
+			if (value === prop) {
+				let crypted = CryptoJS.AES.decrypt(
+					item[prop].toString(),
+					process.env.CRYPTO_KEY,
+				);
+				item[prop] = crypted.toString(CryptoJS.enc.Utf8);
+			}
+		}
+	}
 
-        // Return crypted output
-        return output.toString();
-    }
-
-    const decryptData = (item, ...inputs) => {
-        // Loop on values
-        for( let value of inputs ){
-            // Loop on object
-            for( let prop in item ){
-                if(value === prop){
-                    let crypted  = CryptoJS.AES.decrypt(item[prop].toString(), process.env.CRYPTO_KEY);
-                    item[prop] = crypted.toString(CryptoJS.enc.Utf8);
-                }
-            }
-        }
-
-        // Return decrypted item
-        return item;
-    }
-// 
-
+	// Return decrypted item
+	return item;
+};
+//
 
 /*
 Export service fonctions
 */
-    module.exports = {
-        cryptData,
-        decryptData
-    };
+module.exports = {
+	cryptData,
+	decryptData,
+};
 //
